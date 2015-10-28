9a57bbfaf99eaae5053dd6f4d6c7cbe3ddbc8f79
fkorsa <fkorsa31@gmail.com>
2015-10-28 20:38:49 +0100

    Added timer between blind tests Displaying the keys Saving folder between uses

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -5,12 +5,19 @@
 #include <QDirIterator>
 #include <QDebug>
 #include <QKeyEvent>
+#include <QTime>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::MainWindow)
+    ui(new Ui::MainWindow),
+    remainingTime(0),
+    currentlyPlaying(false)
 {
     ui->setupUi(this);
+
+    ui->playerLabel1->setText("Player 1");
+    ui->playerLabel2->setText("Player 2");
+    ui->announcementDisplay->setFontPointSize(15);
 
     QObject::connect(ui->browseButton, SIGNAL(clicked()), this, SLOT(onBrowseClicked()));
     QObject::connect(ui->playButton, SIGNAL(clicked()), this, SLOT(onPlayClicked()));
@@ -20,10 +27,24 @@ MainWindow::MainWindow(QWidget *parent) :
     mediaPlayer = new QMediaPlayer;
     mediaPlayer->setVolume(50);
 
+    timer = new QTimer(this);
+    connect(timer, SIGNAL(timeout()), this, SLOT(onTimerTick()));
+
     answerWidgetsList.append(ui->answerWidget1);
     answerWidgetsList.append(ui->answerWidget2);
     answerWidgetsList.append(ui->answerWidget3);
     answerWidgetsList.append(ui->answerWidget4);
+
+    QFile file("settings.pbt");
+    if (file.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        QTextStream in(&file);
+        QString line = in.readLine();
+        setDirectory(line);
+        file.close();
+    }
+
+    qsrand(QTime::currentTime().msec());
 
     grabKeyboard();
 }
@@ -31,27 +52,22 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+    delete timer;
 
     delete mediaPlayer;
 }
 
 void MainWindow::onBrowseClicked()
 {
-    QString musicDirectory;
-    ui->playButton->setEnabled(true);
-    ui->stopButton->setEnabled(false);
-    musicsList.clear();
-    musicDirectory = QFileDialog::getExistingDirectory();
-    QDirIterator it(musicDirectory, QDirIterator::Subdirectories);
-    while (it.hasNext())
+    QString directory = QFileDialog::getExistingDirectory();
+    setDirectory(directory);
+    QFile file("settings.pbt");
+    if (file.open(QIODevice::WriteOnly | QIODevice::Text))
     {
-        QFileInfo musicPath(it.next());
-        if(musicPath.isFile() && musicPath.completeSuffix().compare("mp3") == 0)
-        {
-            musicsList.append(musicPath);
-        }
+        QTextStream out(&file);
+        out << directory;
+        file.close();
     }
-    ui->musicDirectoryWidget->setText(QFileInfo(musicDirectory).baseName());
 }
 
 void MainWindow::onPlayClicked()
@@ -60,6 +76,9 @@ void MainWindow::onPlayClicked()
     {
         scoreP1 = 0;
         scoreP2 = 0;
+        playedMusicsCount = 0;
+        playedMusicsList.fill(false, musicsList.size());
+        currentlyPlaying = true;
 
         nextMusic();
     }
@@ -70,6 +89,7 @@ void MainWindow::onStopClicked()
     mediaPlayer->stop();
     ui->stopButton->setEnabled(false);
     ui->playButton->setEnabled(true);
+    currentlyPlaying = false;
 }
 
 void MainWindow::onVolumeSliderChanged(int volume)
@@ -77,46 +97,74 @@ void MainWindow::onVolumeSliderChanged(int volume)
     mediaPlayer->setVolume(volume);
 }
 
+void MainWindow::onTimerTick()
+{
+    remainingTime--;
+    if(remainingTime == 1)
+    {
+        ui->announcementDisplay->setText(announcementText + "1 second before the next music...");
+    }
+    else
+    {
+        ui->announcementDisplay->setText(announcementText + QString::number(remainingTime) + " seconds before the next music...");
+    }
+
+    if(remainingTime <= 0)
+    {
+        ui->announcementDisplay->setText("");
+        timer->stop();
+        nextMusic();
+    }
+}
+
 void MainWindow::keyPressEvent(QKeyEvent *event)
 {
     int player;
     int playerAnswer;
+    if(remainingTime > 0 || !currentlyPlaying)
+    {
+        return;
+    }
     switch(event->key())
     {
     case Qt::Key_1:
         playerAnswer = 0;
-        player = 1;
+        player = 2;
         break;
     case Qt::Key_2:
         playerAnswer = 1;
-        player = 1;
+        player = 2;
         break;
     case Qt::Key_3:
         playerAnswer = 2;
-        player = 1;
+        player = 2;
         break;
     case Qt::Key_4:
         playerAnswer = 3;
-        player = 1;
+        player = 2;
         break;
     case Qt::Key_F1:
         playerAnswer = 0;
-        player = 2;
+        player = 1;
         break;
     case Qt::Key_F2:
         playerAnswer = 1;
-        player = 2;
+        player = 1;
         break;
     case Qt::Key_F3:
         playerAnswer = 2;
-        player = 2;
+        player = 1;
         break;
     case Qt::Key_F4:
         playerAnswer = 3;
-        player = 2;
+        player = 1;
         break;
     default:
         break;
+    }
+    if(playerAnswer > musicsList.size() - 1)
+    {
+        return;
     }
     if(playerAnswer == rightAnswerPosition)
     {
@@ -129,7 +177,11 @@ void MainWindow::keyPressEvent(QKeyEvent *event)
             scoreP2++;
         }
         updateScores();
-        nextMusic();
+        announcementText = "Player " + QString::number(player) + " won!\n"
+                + "The answer was: " + rightAnswerName + "\n"
+                + "Please wait ";
+        ui->announcementDisplay->setText(announcementText + "3 seconds before the next music...");
+        launchTimer();
     }
     else
     {
@@ -145,9 +197,48 @@ void MainWindow::keyPressEvent(QKeyEvent *event)
     }
 }
 
+void MainWindow::setDirectory(QString musicDirectory)
+{
+    ui->playButton->setEnabled(true);
+    ui->stopButton->setEnabled(false);
+    musicsList.clear();
+
+    QDirIterator it(musicDirectory, QDirIterator::Subdirectories);
+    while (it.hasNext())
+    {
+        QFileInfo musicPath(it.next());
+        if(musicPath.isFile() && musicPath.completeSuffix().compare("mp3") == 0)
+        {
+            musicsList.append(musicPath);
+        }
+    }
+    ui->musicDirectoryWidget->setText(QFileInfo(musicDirectory).baseName());
+}
+
+void MainWindow::launchTimer()
+{
+    remainingTime = 3;
+    timer->start(1000);
+}
+
 void MainWindow::nextMusic()
 {
     int musicIndex = qrand() % musicsList.size();
+    while(playedMusicsList[musicIndex])
+    {
+        musicIndex++;
+        if(musicIndex >= playedMusicsList.size())
+        {
+            musicIndex = 0;
+        }
+    }
+    playedMusicsList[musicIndex] = true;
+    playedMusicsCount++;
+    if(playedMusicsCount >= musicsList.size())
+    {
+        playedMusicsList.fill(false, musicsList.size());
+        playedMusicsCount = 0;
+    }
     mediaPlayer->setMedia(QUrl::fromLocalFile(musicsList[musicIndex].absoluteFilePath()));
 
     mediaPlayer->play();
@@ -158,6 +249,7 @@ void MainWindow::nextMusic()
     int randomAnswersCount = 0;
 
     rightAnswerPosition = qrand() % 4;
+    rightAnswerName = musicsList[musicIndex].baseName();
     answerWidgetsList[rightAnswerPosition]->setText(musicsList[musicIndex].baseName());
     musicsSelected[musicIndex] = true;
 
@@ -183,6 +275,6 @@ void MainWindow::nextMusic()
 
 void MainWindow::updateScores()
 {
-    ui->labelScore1->setText(QString::number(scoreP1));
-    ui->labelScore2->setText(QString::number(scoreP2));
+    ui->playerScore1->display(scoreP1);
+    ui->playerScore2->display(scoreP2);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -5,6 +5,7 @@
 #include <QFileInfo>
 #include <QMediaPlayer>
 #include <QTextBrowser>
+#include <QTimer>
 
 namespace Ui {
 class MainWindow;
@@ -23,20 +24,30 @@ public slots:
     void onPlayClicked();
     void onStopClicked();
     void onVolumeSliderChanged(int volume);
+    void onTimerTick();
 
 protected:
     void keyPressEvent(QKeyEvent * event) override;
 
 private:
+    void setDirectory(QString musicDirectory);
+    void launchTimer();
     void nextMusic();
     void updateScores();
 
     Ui::MainWindow *ui;
     QVector<QFileInfo> musicsList;
+    QVector<bool> playedMusicsList;
     QMediaPlayer *mediaPlayer;
     QVector<QTextBrowser*> answerWidgetsList;
+    QTimer *timer;
+    QString rightAnswerName;
+    QString announcementText;
     int rightAnswerPosition;
     int scoreP1, scoreP2;
+    int playedMusicsCount;
+    int remainingTime;
+    bool currentlyPlaying;
 };
 
 #endif // MAINWINDOW_H
